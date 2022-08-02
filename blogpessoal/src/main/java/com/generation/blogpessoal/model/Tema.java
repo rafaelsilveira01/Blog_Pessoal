5c477b65c87242efdcf598d6e30c1ab59f715c6a
Rafael Silveira <rafaell.ssouza@hotmail.com>
2022-08-01 22:48:51 -0300

    criado recurso tema

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,40 @@
+package com.generation.blogpessoal.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank // Preenchimento obrigatorio
+	@Size(min = 100 , max = 1000) // Validar os caracteres
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -6,11 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.model.Tema;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao")String descricao);
 
-	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+	
+
+	
+
 
 }
